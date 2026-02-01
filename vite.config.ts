99330ae561fbaeaf1fb196d57469df1e9909cfed
Honey Kalekar <honey.kalekar.batch2024@sitnagpur.siu.edu.in>
2026-02-01 16:28:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [
     react(),
